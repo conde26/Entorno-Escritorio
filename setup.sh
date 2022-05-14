d32e01315b9c71cadd3501dee572b0b017137865
Conde <79221088+conde26@users.noreply.github.com>
2022-05-15 00:15:56 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -38,8 +38,8 @@ chsh -s $(which zsh)
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
-cp zshrc ~.zshrc
-cp p10k/p10k.zsh ~.p10k.zsh
+cp zshrc ~/.zshrc
+cp p10k/p10k.zsh ~/.p10k.zsh
 
 #Extra Tools
 wget -q -O htmlq.tar.gz https://github.com/mgdm/htmlq/releases/latest/download/htmlq-x86_64-linux.tar.gz 
