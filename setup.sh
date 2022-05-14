5a6a5a4120598aa8e35f7f1703eed95a472b25be
Conde <79221088+conde26@users.noreply.github.com>
2022-05-15 00:04:21 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -3,7 +3,7 @@
 
 #Owner 
 sudo chown -R $(whoami): * 
-sudo chown -R $(whoami): .zshrc 
+sudo chown -R $(whoami): zshrc 
 
 #Requeriments and tools
 sudo apt-get update
@@ -38,8 +38,8 @@ chsh -s $(which zsh)
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
-cp .zshrc ~
-cp p10k/.p10k.zsh ~
+cp zshrc ~.zshrc
+cp p10k/p10k.zsh ~.p10k.zsh
 
 #Extra Tools
 wget -q -O htmlq.tar.gz https://github.com/mgdm/htmlq/releases/latest/download/htmlq-x86_64-linux.tar.gz 
